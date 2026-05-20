20de4c4002b66784616fb48a7b7f19596ec4e821
Lina Hall <liina00girl@hotmail.com>
2026-05-20 19:00:17 +0200

    Added, Game and Genre entities to domain layer

---
+++ GameStore.Domain/Entities/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public class Game//DOMAIN ENTITY represent "A" game in store
+    {
+        public int Id { get; set; } //primary key 
+        public string Title { get; set; } = string.Empty;//the name of game
+        public decimal Price { get; set; }//price of game
+        public string Description { get; set; } = string.Empty;//description of game
+        public int ReleaseYear { get; set; }// Release year of game
+        public int GenreId { get; set; }//foreign key, links the "Game" to a genre.
+        public Genre Genre { get; set; } = null; //never null when running, "called" navigation property, use to gain acess to details from GENRE
+    }
+}
---
+++ GameStore.Domain/Entities/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public class Genre//DOMAIN ENTITY it represent "A" game genre
+    {
+        public int Id { get; set; } //This will be primary key, it is
+        public string Name { get; set; } = string.Empty; //Genre name, rpg fps.
+
+        public ICollection<Game> Games { get; set; } = new List<Game>();//one to many relationship, one genre can have many games.
+    }
+}
